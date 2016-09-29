732de88f09fc402f06d695a79d2fd98ccaf0648f
Morwan <morwanpl@gmail.com>
2016-09-29 20:31:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,52 @@
-var pics = ["pics/a.jpg","pics/b.jpg","pics/c.jpg","pics/d.jpg","pics/e.jpg","pics/f.jpg","pics/g.jpg","pics/h.jpg","pics/i.jpg","pics/j.jpg","pics/k.jpg","pics/l.jpg","pics/m.jpg"]
-
-var i=0;
-
-$(document).ready(function(){
-		$('.picture').click(
+$(document).ready(function()
+	{
+		if($('body').is('#index'))
+		var pics = ["pics/1.jpg","pics/2.jpg","pics/3.jpg"];
+		
+		else if($('body').is('#illustrations'))
+		var pics = ["pics/1.jpg","pics/2.jpg","pics/3.jpg","pics/4.jpg","pics/5.jpg","pics/6.jpg","pics/7.jpg","pics/8.jpg","pics/9.jpg","pics/10.jpg","pics/11.jpg","pics/12.jpg","pics/13.jpg"];
+		
+		else if($('body').is('#storyboards'))
+		var pics = [];
+		
+		var i=0;
+	
+		$('.thumb').click(
 		function()
 		{
 			$('#slider').show();
-			$('#picplace').html("<img id='newpic' src='" + $(this).css('background-image').substring(30,40) + "'/>");
-		
+			$('#photo').css("background-image","pics/" + $(this).css("background-image").substring(56,61));
+			
 			for(var j=0;j<pics.length;j++)
-			if( ($(this).css('background-image').substring(30,40)) == pics[j] )
+			if(($(this).css("background-image").substring(56,61)))
 			i=j;
 		})
 		
-		$('#right,#picplace').click(
+		$('#right,#photo').click(
 		function()
 		{	
 			if(i<pics.length-1)
 			i++;
 			else
 			i=0;
-		$('#picplace').html("<img id='newpic' src='" + pics[i] + "'/>");
+		$('#photo').attr('src',pics[i]);
 		})
 		
 		$('#left').click(
 		function()
 		{	
-			if(i>1)
+			if(i>0)
 			i--;
 			else
 			i=pics.length-1;
-		$('#picplace').html("<img id='newpic' src='" + pics[i] + "'/>");
+		$('#photo').attr('src',pics[i]);
 		})
-})
-
-$(document).on("click","#x",function(){
+	})
+$(document).on("click","#x,#background",function(){
 		$('#slider').hide();
-		$('.picture').click(
+		$('.thumb').click(
 		function()
 		{
 				$('#slider').show();
 		})
-})
-
+	})
