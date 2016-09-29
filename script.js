952a1f919d795d72843728ee653e14390ab835d2
Morwan <morwanpl@gmail.com>
2016-09-29 20:33:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(document).ready(function()
 	{
 		if($('body').is('#index'))
-		var pics = ["pics/1.jpg","pics/2.jpg","pics/3.jpg"];
+		var pics = ["pics/01.jpg","pics/02.jpg","pics/03.jpg"];
 		
 		else if($('body').is('#illustrations'))
-		var pics = ["pics/1.jpg","pics/2.jpg","pics/3.jpg","pics/4.jpg","pics/5.jpg","pics/6.jpg","pics/7.jpg","pics/8.jpg","pics/9.jpg","pics/10.jpg","pics/11.jpg","pics/12.jpg","pics/13.jpg"];
+		var pics = ["pics/01.jpg","pics/02.jpg","pics/03.jpg","pics/04.jpg","pics/05.jpg","pics/06.jpg","pics/07.jpg","pics/08.jpg","pics/09.jpg","pics/10.jpg","pics/11.jpg","pics/12.jpg","pics/13.jpg"];
 		
 		else if($('body').is('#storyboards'))
 		var pics = [];
@@ -15,10 +15,10 @@ $(document).ready(function()
 		function()
 		{
 			$('#slider').show();
-			$('#photo').css("background-image","pics/" + $(this).css("background-image").substring(56,61));
+			$('#photo').css("background-image","pics/" + $(this).css("background-image").substring(56,62));
 			
 			for(var j=0;j<pics.length;j++)
-			if(($(this).css("background-image").substring(56,61)))
+			if(($(this).css("background-image").substring(56,62)))
 			i=j;
 		})
 		
